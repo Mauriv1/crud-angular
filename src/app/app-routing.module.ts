5ea190c3713d9bab190f4ebb0e738d5bee098fd5
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-10-09 16:41:40 -0500

    config-project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/add/add.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: 'add'},
+  {path: 'add', component: AddComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'edit', component: EditComponent},
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
