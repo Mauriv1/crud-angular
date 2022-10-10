177c3604f5938f7c06d762865ce6f463eeae0937
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-10-10 09:25:17 -0500

    commit-components

--- php/indexphp.php
+++ php/indexphp.php
@@ -9,17 +9,42 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $servidor = "localhost"; $usuario = "root"; $contrasenia = "982109821"; $nombreBaseDatos = "crudAngular";
 $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 
-//Inserta un nuevo registro y recepciona en método post los datos de nombre y correo
+//Inserta un nuevo registro y recepciona en método post los datos de descripcion y codigo
+if(isset($_GET["add"])){
+    $data = json_decode(file_get_contents("php://input"));
+    $descripcion=$data->descripcion;
+    $codigo=$data->codigo;
+        if(($codigo!="")&&($descripcion!="")){
 
-// Consulta datos y recepciona una clave para listar dichos datos con dicha clave
+    $sqlcrudAngular = mysqli_query($conexionBD,"INSERT INTO crudAngular(descripcion,codigo) VALUES('$descripcion','$codigo') ");
+    echo json_encode(["success"=>1]);
+        }
+    exit();
+}
+
+// Consulta todos los registros de la tabla crudAngular
+$sqlcrudAngular = mysqli_query($conexionBD,"SELECT * FROM crudAngular ");
+if(mysqli_num_rows($sqlcrudAngular) > 0){
+    $crudAngular = mysqli_fetch_all($sqlcrudAngular,MYSQLI_ASSOC);
+    echo json_encode($crudAngular);
+}
+else{ echo json_encode([["success"=>0]]); 
+}
 
 //borrar pero se le debe de enviar una clave ( para borrado )
-
+if (isset($_GET["borrar"])){
+    $sqlEmpleaados = mysqli_query($conexionBD,"DELETE FROM empleados WHERE id=".$_GET["borrar"]);
+    if($sqlEmpleaados){
+        echo json_encode(["success"=>1]);
+        exit();
+    }
+    else{  echo json_encode(["success"=>0]); }
+}
 
 
 // Actualiza datos pero recepciona datos de nombre, correo y una clave para realizar la actualización
 
-// Consulta todos los registros de la tabla crudAngular
+// Consulta datos y recepciona una clave para listar los datos con dicha clave
 
 
 ?>
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './components/add/add.component';
 import { DeleteComponent } from './components/delete/delete.component';
+import { DetailComponent } from './components/detail/detail.component';
 import { EditComponent } from './components/edit/edit.component';
 import { ListComponent } from './components/list/list.component';
 
@@ -12,7 +13,7 @@ const routes: Routes = [
   {path: 'list', component: ListComponent},
   {path: 'edit', component: EditComponent},
   {path: 'delete', component: DeleteComponent},
-  
+  {path: 'detail', component: DetailComponent},
 ];
 
 @NgModule({
